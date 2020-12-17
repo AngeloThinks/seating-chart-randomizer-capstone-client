968b2e5a020b7f0e781be66b9a3c028854006949
Angelo Vazquez <angelovazquez@Angelos-MBP.lan1>
2020-12-17 00:19:05 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  API_ENDPOINT: "https://afternoon-inlet-82835.herokuapp.com/",
+  API_ENDPOINT: "https://afternoon-inlet-82835.herokuapp.com",
   // API_ENDPOINT: "http://localhost:8000",
 };
